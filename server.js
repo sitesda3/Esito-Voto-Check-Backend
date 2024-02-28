68cd3134b0d46820046dfec646ca84e0e9df5ebd
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2024-02-28 09:46:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ async function fetchData(codicePersonale, password) {
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "domcontentloaded" });
   
-  await page.waitForSelector("#login").then(() => page.type("#login", codicePersonale));
+  // await page.waitForSelector("#login").then(() => page.type("#login", codicePersonale));
 
   await page.waitForSelector("#password");
   await page.type("#password", await password);
