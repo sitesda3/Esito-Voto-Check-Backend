c22cee0688ad5184ce62a2a742dab30e8503f1e2
sitesda3 <86269726+sitesda3@users.noreply.github.com>
2024-03-27 17:42:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,56 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!!");
+
+
+
+console.log("start fetchData");
+  const url = "https://web.spaggiari.eu/cvv/app/default/genitori_voti.php";
+  const browser = await puppeteer.launch(
+    { headless: "new" },
+    {
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+        "--proxy-server=http://86.48.0.127:3128",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    }
+  );
+  const page = await browser.newPage();
+  console.log("page creata");
+
+  //Randomize viewport size
+  await page.setViewport({
+    width: 1920 + Math.floor(Math.random() * 100),
+    height: 3000 + Math.floor(Math.random() * 100),
+    deviceScaleFactor: 1,
+    hasTouch: false,
+    isLandscape: false,
+    isMobile: false,
+  });
+
+
+        await page.goto('https://www.google.com', {waitUntil: 'networkidle2'});
+
+        // Login by Fill User / Password
+        //await page.type('#Email', email);
+        //await page.type('#Password', password);
+        //await page.$eval('button[type=submit]', el => el.click());
+        await page.waitForNetworkIdle({idleTime: idelTime});
+
+        const screenshot = await page.screenshot();
+    console.log('screenshot done');
+        res.end(screenshot, 'binary');
+      return;
+
+
+  
 });
 
 app.listen(PORT, () => {
