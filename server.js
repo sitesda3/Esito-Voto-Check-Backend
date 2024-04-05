9a330c89290431a32cc162457b0a987bc2cbd35b
sitesda3 <86269726+sitesda3@users.noreply.github.com>
2024-04-05 23:52:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -59,6 +59,8 @@ const launchOptions = {
     args: [
         '--no-sandbox',
         '--disable-web-security',
+        '--disable-xss-auditor',//
+	'--disable-webgl',//
         '--disable-gpu',
         '--disable-dev-shm-usage',
         '--disable-setuid-sandbox',
@@ -72,6 +74,43 @@ const launchOptions = {
         //'--disable-infobars',
         //'--window-size=1366,768'
          //'--single-process',
+
+'--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  /*'--disable-dev-shm-usage',*/
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  /*'--disable-setuid-sandbox',*/
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  /*'--no-first-run',*/
+  '--no-pings',
+  /*'--no-sandbox',*/
+  /*'--no-zygote',*/
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain'
+	    
     ],
   executablePath:
         process.env.NODE_ENV === "production"
